f03255aabe9e8b30fadc956f71cd545a2cf15610
raven <enn.raven@gmail.com>
2013-08-29 22:14:13 +0800

    Initialize project and half baked homepage.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PureMind::Application.config.secret_token = 'c01f4cd723af9097671978058145bf3fb7d07723e7dc6df12c94b204caa7fd0461e971e9fca8e0eb66bd5b1ab5d47bc902214ef6ad6f20beb53279074f3f8a42'
